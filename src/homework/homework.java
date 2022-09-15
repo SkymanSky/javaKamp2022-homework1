7c7cc4272d5ba81471ba345e12d9c65dbaafe128
SkymanSky <gokera@ogr.iu.edu.tr>
2022-09-15 22:38:23 +0300

    datatypes.

--- src/homework/homework.java
+++ src/homework/homework.java
@@ -9,6 +9,22 @@ public class homework {
 		int ogranciSayisi=12;
 		String mesaj="Öğrenci sayısı: ";
 		System.out.println(mesaj+ogranciSayisi);
+		System.out.println(mesaj+ogranciSayisi);
+		System.out.println("Öğrenci sayım: "+ogranciSayisi);
+		System.out.println("Öğrenci sayım: "+ogranciSayisi);
+		System.out.println("Öğrenci sayım: "+ogranciSayisi);
+		
+		//datatypes
+		byte sayi1=121;
+		double sayi2=12.5;	
+		int sayi=12;
+		sayi=1211111111;
+		
+		char karakter='A';
+		
+		boolean dogruMu=false;
+		
+				
 
 	}
 
