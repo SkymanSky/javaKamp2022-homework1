557649425b3e58d3808ad18e60311cb6dc61f375
SkymanSky <gokera@ogr.iu.edu.tr>
2022-09-21 20:44:41 +0300

    Mini Proje 4 - Arkadaş Sayılar

--- src/homework/homework.java
+++ src/homework/homework.java
@@ -253,8 +253,10 @@ public class homework {
 			System.out.println("Sayı asal değildir.");
 		}
 */
+		
+
 		// Mini Proje 2 - Kalın Sesli ve İnce Sesli Harfler
-		char harf = 'I';
+/*		char harf = 'I';
 
 		switch (harf) {
 		case 'A':
@@ -267,9 +269,10 @@ public class homework {
 			System.out.println("İnce sesli harf.");
 
 		}
+*/
 
 		// Mini Proje 3 - Mükemmel Sayılar
-		int number2 = 28;
+/*		int number2 = 28;
 		int total2 = 0;
 
 		for (int a = 1; a < number2; a++) {
@@ -283,9 +286,42 @@ public class homework {
 		} else {
 			System.out.println("Mükemmel sayı değildir.");
 		}
-	}
-
+*/
 		
+
+		//Mini Proje 4 - Arkadaş Sayılar
+		int sayi1=222;
+		int sayi2=284;
+		int toplam1=0;
+		int toplam2=0;
+		
+		for (int i = 1; i < sayi1; i++) {
+			if(sayi1%i==0) {
+				toplam1=toplam1+i;
+			}
+			
+		}
+		
+		for (int i = 1; i < sayi2; i++) {
+			if(sayi2%i==0) {
+				toplam2=toplam2+i;
+			}
+			
+		}
+		
+		if(sayi1==toplam2 && sayi2==toplam1) {
+			System.out.println("Bu iki sayı arkadaştır.");
+		}else {
+			System.out.println("Bu iki sayı arkadaş değildir.");
+		}
+		
+		
+		
+		
+		
+		
+	
+	}
 
 }
 
